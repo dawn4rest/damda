e2093a3f942beca044d0935e58ea65907804cc02
한재협 <dawn4rest@hanjaehyeob-ui-MacBookAir.local>
2023-04-26 14:59:26 +0900

    feat: Finish customizing

--- damda/ai/static/js/script.js
+++ damda/ai/static/js/script.js
@@ -1,1 +1,5 @@
-
+$('body').keyup(function(e){
+  if (e.keyCode === 32 && $('.space-link').length) {
+    window.location.href = $('.space-link').attr('href');
+  }
+});
--- damda/ai/views.py
+++ damda/ai/views.py
@@ -1,43 +1,107 @@
-from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, redirect, HttpResponse
 from django.http import StreamingHttpResponse
-from django.views import generic
-from django.template.loader import get_template 
-from django.urls import reverse
-from random import randint
 
+import random
 import cv2, time
 import threading
 
-from .models import Dam
-
 
 def index(request):
-    context = {
-      "bg_type": randint(0,7)
-    }
-    return render(request, "index.html", context)
+    request.session['red'] = 255
+    request.session['green'] = 255
+    request.session['blue'] = 255
+    context = {'bg_type': random.randint(0,7)}
+    return render(request, 'index.html', context)
 
 
 def name_form(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         username = request.POST.get('username')
         if username != '':
             request.session['username'] = username
             return redirect('ai:name-result')
 
-    return render(request, "name_form.html")
+    return render(request, 'name_form.html')
 
 
 def name_result(request):
+    context = {'username': request.session.get('username')}
+    return render(request, 'name_result.html', context)
+
+
+def name_result(request):
+    context = {'username': request.session.get('username')}
+    return render(request, 'name_result.html', context)
+
+
+def feel_form(request):
+    return render(request, 'feel_form.html')
+
+
+def feel_result(request):
+    context = {'username': request.session.get('username')}
+    return render(request, 'feel_result.html', context)
+
+
+def problem_form(request):
+    if request.method == 'POST':
+        problem = request.POST.get('problem')
+        if problem != '':
+            request.session['problem'] = problem
+            return redirect('ai:problem-result')
+
+    return render(request, 'problem_form.html')
+
+
+def problem_result(request):
+    return render(request, 'problem_result.html')
+
+
+def face01_form(request):
+    return render(request, 'face01_form.html')
+
+
+def face01_result(request):
+    request.session['red'] = random.randint(0,255)
+    request.session['green'] = random.randint(0,255)
+    request.session['blue'] = random.randint(0,255)
+
+    request.session['emotion01'] = random.choice([True, False])
+    context = {'emotion01': request.session['emotion01']}
+    return render(request, 'face01_result.html', context)
+
+
+def goal_form(request):
+    return render(request, 'goal_form.html')
+
+
+def goal_result(request):
+    return render(request, 'goal_result.html')
+
+
+def face02_form(request):
+    return render(request, 'face02_form.html')
+
+
+def face02_result(request):
+    request.session['emotion02'] = random.choice([True, False])
     context = {
-      "username": request.session.get('username')
+      'emotion01': request.session['emotion01'],
+      'emotion02': request.session['emotion02']
     }
-    return render(request, "name_result.html", context)
+    return render(request, 'face02_result.html', context)
 
 
-def face(request):
-    template = get_template('face.html')
-    return HttpResponse(template.render({}, request))
+def canvas_intro(request):
+    return render(request, 'canvas_intro.html')
+
+
+def canvas_result(request):
+    context = {
+      'hex_color': '%02x%02x%02x' % (request.session['red'],request.session['green'],request.session['blue']),
+      'quote_index': random.randint(0,56)
+    }
+    return render(request, 'canvas_result.html', context)
 
 
 def stream():
@@ -47,7 +111,7 @@ def stream():
         ret, frame = cap.read()
 
         if not ret:
-            print("Error: failed to capture image")
+            print('Error: failed to capture image')
             break
 
         cv2.imwrite('demo.jpg', frame)
@@ -60,4 +124,4 @@ def video(request):
             time.sleep(2)
             return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
         except HttpResponseServerError as e:
-            print("asborted", e)
+            print('asborted', e)
